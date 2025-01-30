7297294ceb75832f24f956dfbefae6870dbaefbd
AmrSaleh2234 <amrsaleh1001@gmail.com>
2025-01-30 12:48:07 +0300

    add validations for send email in forget password

--- modules/Auth/Controllers/AuthController.php
+++ modules/Auth/Controllers/AuthController.php
@@ -20,9 +20,10 @@ use Modules\User\Presenters\UserPresenter;
 class AuthController extends Controller
 {
     public function __construct(
-        private AuthService $authService,
+        private AuthService    $authService,
         private MakeOtpHandler $makeOtpHandler,
-    ) {
+    )
+    {
     }
 
     public function login(LoginRequest $request)
@@ -47,12 +48,12 @@ class AuthController extends Controller
         try {
             [$token, $user] = $this->authService->loginWithOtp($request->createLoginDTO());
         } catch (\Exception $e) {
-            return Json::buildItems(data:["message" => $e->getMessage()], httpStatus: 403);
+            return Json::buildItems(data: ["message" => $e->getMessage()], httpStatus: 403);
         }
 
         $userPresenter = (new UserPresenter($user))->getData();
 
-        return Json::buildItems(data:["message" => "success", "token" => $token, "user" => $userPresenter]);
+        return Json::buildItems(data: ["message" => "success", "token" => $token, "user" => $userPresenter]);
     }
 
 
@@ -60,38 +61,43 @@ class AuthController extends Controller
     {
         $this->authService->logout();
 
-        return Json::buildItems(data:["message" => "success"]);
+        return Json::buildItems(data: ["message" => "success"]);
     }
 
     public function forgetPassword(ForgetPasswordRequest $request)
     {
         $command = $request->createForgetPasswordCommand();
-        $this->makeOtpHandler->handle($command);
+        try {
+            $this->makeOtpHandler->handle($command);
 
+        } catch (\Exception $e) {
+            return Json::buildItems(data: ["msg" => $e->getMessage()], httpStatus: 400);
+        }
 
         return Json::buildItems(null, ["message" => "success"], "", 200);
     }
 
-    public function resetPassword(ResetPasswordRequest $request)
+    public
+    function resetPassword(ResetPasswordRequest $request)
     {
         try {
             $this->authService->ResetPassword($request->createResetPasswordCommand());
         } catch (\Exception $e) {
-            return Json::buildItems(data: ["message" => $e->getMessage()], httpStatus:  401);
+            return Json::buildItems(data: ["message" => $e->getMessage()], httpStatus: 401);
         }
 
         return Json::buildItems(data: ["message" => "success"]);
     }
 
-    public function resendOtp(ResendOtpRequest $resendOtpRequest)
+    public
+    function resendOtp(ResendOtpRequest $resendOtpRequest)
     {
         $command = $resendOtpRequest->createResendOtpCommand();
         try {
             $this->authService->resendOtp($command);
 
-        }
-        catch (\Exception $e) {
-            return Json::buildItems(data: ["msg" => $e->getMessage()], httpStatus:  400);
+        } catch (\Exception $e) {
+            return Json::buildItems(data: ["msg" => $e->getMessage()], httpStatus: 400);
         }
         return Json::buildItems(data: ["msg" => "success"]);
 
