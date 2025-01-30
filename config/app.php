7dc7f8bcc919fccc672466aa835d3e9d531a9227
AmrSaleh2234 <amrsaleh1001@gmail.com>
2025-01-30 09:26:53 +0300

    solve some comments

--- config/app.php
+++ config/app.php
@@ -79,7 +79,6 @@ return [
     */
 
     'locale' => env('APP_LOCALE', 'en'),
-    'continue_with_otp' => env('CONTINUE_WITH_OTP', 0),
 
     "available_locales" => ["en", "ar"],
     'fallback_locale' => env('APP_FALLBACK_LOCALE', 'en'),
