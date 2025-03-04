2b45ec3da48cf4b457d4965c9ef4e97a45d45226
AmrSaleh2234 <amrsaleh1001@gmail.com>
2025-03-04 22:44:00 +0300

    CRUD counrty add missed property status and sms driver

--- modules/Setting/Handlers/AnswerQuestionForUserHandler.php
+++ modules/Setting/Handlers/AnswerQuestionForUserHandler.php
@@ -6,16 +6,12 @@ namespace Modules\Setting\Handlers;
 
 use Modules\Auth\Repositories\VerficationQuestionRepository;
 use Modules\Setting\Commands\Question\AnswerQuestionsByUserCommand;
-use Modules\Setting\Commands\UpdateLoginWayCommand;
-use Modules\Setting\Repositories\LoginWayRepository;
-use Modules\Setting\Repositories\SettingRepository;
-use Ramsey\Uuid\UuidInterface;
+
 
 class AnswerQuestionForUserHandler
 {
     public function __construct(
         private VerficationQuestionRepository $verficationQuestionRepository,
-
     ) {
     }
 
