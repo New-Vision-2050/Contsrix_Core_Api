de9ba57efe983193df24c17bb28b86a05eeae28d
AmrSaleh2234 <amrsaleh1001@gmail.com>
2025-03-11 03:03:29 +0300

    make attempts bigger

--- modules/Auth/Resources/routes/api.php
+++ modules/Auth/Resources/routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use Modules\Auth\Controllers\AuthController;
-Route::group(['middleware' => ['throttle:5,1']],function (){
+Route::group(['middleware' => ['throttle:13,1']],function (){
     Route::post('/login', [AuthController::class, 'login']);
     Route::post('/login-step', [AuthController::class, 'loginBySteps']);
     Route::post('/login-otp', [AuthController::class, 'loginWithOtp']);
