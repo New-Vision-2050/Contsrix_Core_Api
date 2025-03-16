5c00fc1c40b3e42f4cde9dc4bbc5074a27b27f60
mabou7agar <13303169+mabou7agar@users.noreply.github.com>
2025-03-16 07:36:52 +0200

    Enhance Company User Response

--- modules/CompanyUser/Presenters/CompanyUserPresenter.php
+++ modules/CompanyUser/Presenters/CompanyUserPresenter.php
@@ -30,7 +30,7 @@ class CompanyUserPresenter extends AbstractPresenter
             "phone" => $this->companyUser->phone,
             'job_title_id'=>$this->companyUser->job_title_id,
             "job_title" => $this->companyUser?->jobTitle?->name,
-            "country" => (new CountryPresenter($this->companyUser?->country))->getData(),
+            "country" => (new CountryPresenter($this->companyUser?->country ?? collect([])))->getData(),
             'data_status' => false,
             "company" => CompanyWithRolesPresenter::collection($this->companyUser->companies->unique('id'),$this->companyUser)
         ];
