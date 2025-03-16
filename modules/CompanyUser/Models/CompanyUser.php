0339eaa9a128c477602fa5815e7fe9c3db586c9a
mabou7agar <13303169+mabou7agar@users.noreply.github.com>
2025-03-16 08:23:43 +0200

    Get Roles For Companies

--- modules/CompanyUser/Models/CompanyUser.php
+++ modules/CompanyUser/Models/CompanyUser.php
@@ -83,7 +83,7 @@ class CompanyUser extends Model
 
     public function rolesForCompany($companyId)
     {
-        return $this->companies->where('id',$companyId)->pluck("pivot");
+        return $this->companies()->where('id',$companyId)->orderBy('role_id')->pluck("pivot");
     }
     public function country()
     {
