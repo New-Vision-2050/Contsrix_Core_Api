2a1efcb317a5d676ba64ec312602624a70a63f2d
AmrSaleh2234 <amrsaleh1001@gmail.com>
2025-03-24 02:38:10 +0300

    add update migration

---
+++ modules/CompanyUser/Database/Migrations/2025_03_23_233651_update_company_users_table.php
@@ -0,0 +1,15 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+
+return new class extends Migration
+{
+    public function up()
+    {
+        Schema::table('company_users', function (Blueprint $table) {
+            $table->uuid("global_user_id")->nullable();
+        });
+    }
+};
