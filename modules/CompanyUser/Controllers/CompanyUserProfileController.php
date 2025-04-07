775da9aa138adc8ac4bd04b80268553ff284ec58
Abdelrahman El-Desoky <dev.deesoky@gmail.com>
2025-04-07 09:15:05 +0200

    edit validatePhoto

--- modules/CompanyUser/Controllers/CompanyUserProfileController.php
+++ modules/CompanyUser/Controllers/CompanyUserProfileController.php
@@ -36,7 +36,7 @@ class CompanyUserProfileController extends Controller
     public function validatePhoto(GetCompanyUserRequest $request): JsonResponse
     {
         $errors = $this->companyUserImageValidationService->validateName($request);
-
-       return response()->json($errors);
+        
+        return Json::item($errors);
     }
 }
