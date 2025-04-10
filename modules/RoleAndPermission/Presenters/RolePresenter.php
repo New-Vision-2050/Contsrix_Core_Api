e70b339c59177853ba2167459783c2848c3c8b06
AmrSaleh2234 <amrsaleh1001@gmail.com>
2025-04-10 17:42:57 +0300

    present permission groubing by matching figma

--- modules/RoleAndPermission/Presenters/RolePresenter.php
+++ modules/RoleAndPermission/Presenters/RolePresenter.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Modules\RoleAndPermission\Presenters;
 
+use Modules\RoleAndPermission\Models\Permission;
 use Modules\RoleAndPermission\Models\Role;
 use BasePackage\Shared\Presenters\AbstractPresenter;
 
@@ -18,10 +19,19 @@ class RolePresenter extends AbstractPresenter
 
     protected function present(bool $isListing = false): array
     {
+        $permissions = Permission::get();
+        $modified = [];
+        foreach ($permissions as $permission) {
+            $permission->is_active = $this->role->permissions()->where("name", $permission->name)->first() ? true : false;
+            $modified[] = ["id" => $permission->id, "name" => $permission->name, "is_active" => $permission->is_active];
+        }
+        $modified = collect($modified)->groupBy(function($query) {
+        return explode('.', $query["name"])[0];
+    })->toArray();;
         return [
             'id' => $this->role->id,
             'name' => $this->role->name,
-            "permissions" => PermissionPresenter::collection($this->role->permissions)
+            "permissions" => $modified
         ];
     }
 }
