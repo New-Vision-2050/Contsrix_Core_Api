d63383ba430bd97bbf2a481d57d3e3aacd91fc5e
Abdelrahman El-Desoky <dev.deesoky@gmail.com>
2025-04-14 09:50:11 +0200

    fix identity date end and start

--- modules/CompanyUser/Presenters/CompanyIdentityDataPresenter.php
+++ modules/CompanyUser/Presenters/CompanyIdentityDataPresenter.php
@@ -22,9 +22,20 @@ class CompanyIdentityDataPresenter extends AbstractPresenter
     {
         return [
             'passport' => $this->companyUser->passport,
+            'passport_start_date'=> $this->companyUser->passport_start_date,
+            'passport_end_date'=> $this->companyUser->passport_end_date,
+
             'identity' => $this->companyUser->identity,
+            'identity_start_date'=> $this->companyUser->identity_start_date,
+            'identity_end_date'=> $this->companyUser->identity_end_date,
+
             'border_number' => $this->companyUser->border_number,
+            'border_number_start_date'=> $this->companyUser->border_number_start_date,
+            'border_number_end_date'=> $this->companyUser->border_number_end_date,
+
             'entry_number' => $this->companyUser->entry_number,
+            'entry_number_start_date'=> $this->companyUser->entry_number_start_date,
+            'entry_number_end_date'=> $this->companyUser->entry_number_end_date,
 
             'file_passport' => $this->companyUser->getFirstMedia('file_passport')?->getFullUrl(),
             'file_identity' => $this->companyUser->getFirstMedia('file_identity')?->getFullUrl(),
