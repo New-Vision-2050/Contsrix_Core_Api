84fcf21e70afc52b17a29d3ac74d9d939ba0f04b
Abdelrahman El-Desoky <dev.deesoky@gmail.com>
2025-04-14 09:07:32 +0200

    info company id

--- modules/CompanyUser/Presenters/CompanyUserPresenter.php
+++ modules/CompanyUser/Presenters/CompanyUserPresenter.php
@@ -34,7 +34,10 @@ class CompanyUserPresenter extends AbstractPresenter
             "job_title" => $this->companyUser?->jobTitle?->name,
             "country" => $this->companyUser?->country ? (new CountryPresenter($this->companyUser?->country))->getData() : collect([]),
             'data_status' => 0,
-            "company" => CompanyWithRolesPresenter::collection($this->companyUser->companies->unique('id'),$this->companyUser),
+            "company" => (new CompanyWithRolesPresenter(
+                $this->companyUser->companies->unique('id')->first(),
+                $this->companyUser
+            ))->getData(),
             'Job_role' => '-',
             'date_appointment' => '-',
             'branch'=>'-',
