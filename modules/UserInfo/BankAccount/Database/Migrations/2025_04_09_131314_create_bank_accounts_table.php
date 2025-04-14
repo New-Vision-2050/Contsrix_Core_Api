593fc5681035995b251fe769ad5464801f4fb855
Abdelrahman El-Desoky <dev.deesoky@gmail.com>
2025-04-14 14:37:03 +0200

    fix bank account

--- modules/UserInfo/BankAccount/Database/Migrations/2025_04_09_131314_create_bank_accounts_table.php
+++ modules/UserInfo/BankAccount/Database/Migrations/2025_04_09_131314_create_bank_accounts_table.php
@@ -11,7 +11,6 @@ return new class extends Migration
         Schema::create('bank_accounts', function (Blueprint $table) {
             $table->uuid('id')->primary();
             $table->uuid('company_id')->index();
-            $table->uuid('global_id')->index();
             $table->uuid('country_id')->index();
             $table->uuid('bank_id')->index();
             $table->uuid('currency_id')->index();
