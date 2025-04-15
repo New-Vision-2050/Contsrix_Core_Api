60d96955d25b06c1cc1ba7f6a76a8875c5c0d8b8
Abdelrahman El-Desoky <dev.deesoky@gmail.com>
2025-04-15 12:01:58 +0200

    fix QualificationPresenter

---
+++ modules/Shared/ProfessionalBodie/Database/Seeders/ProfessionalBodieSeeder.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Modules\Shared\ProfessionalBodie\Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Database\Eloquent\Model;
+use Modules\Shared\Language\Models\Language;
+use Modules\Shared\ProfessionalBodie\Models\ProfessionalBodie;
+use Ranium\SeedOnce\Traits\SeedOnce;
+
+class ProfessionalBodieSeeder extends Seeder
+{
+    use SeedOnce;
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Model::unguard();
+
+
+        $specializations = [
+            ['ar' => 'الهيئة السعودية للمهندسين', 'en' => 'Saudi Council of Engineers'],
+            ['ar' => 'نقابة المهندسين', 'en' => 'Engineers Syndicate'],
+        ];
+
+        foreach ($specializations as $index => $item) {
+            ProfessionalBodie::firstOrCreate(
+                ['id' => $index + 1],
+                ['name' => ['en' => $item['en'], 'ar' => $item['ar']]]
+            );
+        }
+    }
+
+}
