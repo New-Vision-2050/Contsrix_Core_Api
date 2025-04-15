e65a6a97fba429c4b43703bf9ccaddf144214d6d
Abdelrahman El-Desoky <dev.deesoky@gmail.com>
2025-04-15 14:48:09 +0200

    rofessionalCertificate

---
+++ modules/UserInfo/ProfessionalCertificate/DTO/CreateProfessionalCertificateDTO.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Modules\UserInfo\ProfessionalCertificate\DTO;
+
+use Ramsey\Uuid\UuidInterface;
+
+class CreateProfessionalCertificateDTO
+{
+    public function __construct(
+        public string $company_id,
+        public string $global_id,
+        public string $professional_bodie_id,
+        public string $accreditation_name,
+        public string $accreditation_number,
+        public string $accreditation_degree,
+        public string $date_obtain,
+        public string $date_end,
+    ) {
+    }
+
+    public function toArray(): array
+    {
+        return [
+            'company_id'=> $this->company_id,
+            'global_id'=> $this->global_id,
+            'professional_bodie_id'=> $this->professional_bodie_id,
+            'accreditation_name'=> $this->accreditation_name,
+            'accreditation_number'=> $this->accreditation_number,
+            'accreditation_degree'=> $this->accreditation_degree,
+            'date_obtain'=> $this->date_obtain,
+            'date_end'=> $this->date_end,
+        ];
+    }
+}
