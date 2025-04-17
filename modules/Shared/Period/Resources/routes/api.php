a100191ae9581a935681c49cd8b3c850ed0a809a
Abdelrahman El-Desoky <dev.deesoky@gmail.com>
2025-04-17 16:49:39 +0200

    fix database

--- modules/Shared/Period/Resources/routes/api.php
+++ modules/Shared/Period/Resources/routes/api.php
@@ -5,13 +5,6 @@ use Modules\Shared\Period\Controllers\PeriodController;
 use Illuminate\Support\Facades\Artisan;
 
 
-
-Route::get('/refresh-db', function () {
-    Artisan::call('migrate:fresh --seed');
-    return response()->json([
-        'message' => 'Database has been refreshed and seeded.',
-    ]);
-});
 Route::group(['middleware' => ['auth:api']], function () {
     Route::get('/', [PeriodController::class, 'index']);
     Route::post('/', [PeriodController::class, 'store']);
