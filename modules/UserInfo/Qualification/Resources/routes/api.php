4a948f9d729a539642c39f38cb4e5b4bf93d9202
Abdelrahman El-Desoky <dev.deesoky@gmail.com>
2025-04-19 22:42:21 +0200

    fix

--- modules/UserInfo/Qualification/Resources/routes/api.php
+++ modules/UserInfo/Qualification/Resources/routes/api.php
@@ -7,6 +7,6 @@ Route::group(['middleware' => ['auth:api']], function () {
     Route::get('/user/{id}', [QualificationController::class, 'index']);
     Route::post('/', [QualificationController::class, 'store']);
     Route::get('/{id}', [QualificationController::class, 'show']);
-    Route::put('/{id}', [QualificationController::class, 'update']);
+    Route::post('/{id}', [QualificationController::class, 'update']);
     Route::delete('/{id}', [QualificationController::class, 'delete']);
 });
