4bd87aa7c9fcde01c401906870b742f6018c6f60
Abdelrahman El-Desoky <dev.deesoky@gmail.com>
2025-04-27 13:43:09 +0300

    make salary type

--- modules/UserInfo/UserSalary/Resources/routes/api.php
+++ modules/UserInfo/UserSalary/Resources/routes/api.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use Modules\UserInfo\UserSalary\Controllers\UserSalaryController;
 
-Route::group(['middleware' => ['auth:api']], function () {
+Route::group(['middleware' => ['auth:api',\Stancl\Tenancy\Middleware\InitializeTenancyByRequestData::class]], function () {
     Route::get('/user/{id}', [UserSalaryController::class, 'index']);
     Route::post('/', [UserSalaryController::class, 'store']);
     Route::get('/{id}', [UserSalaryController::class, 'show']);
