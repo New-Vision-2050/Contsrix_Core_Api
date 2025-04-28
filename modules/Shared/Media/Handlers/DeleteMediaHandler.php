91dfdf167058040a306b799676d8a0bf1656cec1
Abdelrahman El-Desoky <dev.deesoky@gmail.com>
2025-04-28 15:00:39 +0300

    delete one or more files

--- modules/Shared/Media/Handlers/DeleteMediaHandler.php
+++ modules/Shared/Media/Handlers/DeleteMediaHandler.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace Modules\Shared\Media\Handlers;
 
-use Modules\Shared\Media\Repositories\MediaRepository;
+use Modules\CompanyUser\Repositories\MediaRepository;
 use Ramsey\Uuid\UuidInterface;
 
 class DeleteMediaHandler
@@ -14,8 +14,11 @@ class DeleteMediaHandler
     ) {
     }
 
-    public function handle(UuidInterface $id)
+    public function handle($ids)
     {
-        $this->repository->deleteMedia($id);
+        // If a single UUID is passed, make it an array
+        $ids = is_array($ids) ? $ids : [$ids];
+
+        $this->repository->delete($ids);
     }
 }
