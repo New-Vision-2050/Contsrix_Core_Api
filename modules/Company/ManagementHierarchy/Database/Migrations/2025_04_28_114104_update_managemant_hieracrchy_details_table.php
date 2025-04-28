2c91846bd9eaf7da49df7c32bf78ba1269828b17
AmrSaleh2234 <amrsaleh1001@gmail.com>
2025-04-28 14:49:42 +0300

    update relation details

---
+++ modules/Company/ManagementHierarchy/Database/Migrations/2025_04_28_114104_update_managemant_hieracrchy_details_table.php
@@ -0,0 +1,17 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+
+return new class extends Migration
+{
+    public function up()
+    {
+        Schema::table('management_hierarchy_details', function (Blueprint $table) {
+            $table->dropColumn("id");
+            $table->id();
+
+        });
+    }
+};
