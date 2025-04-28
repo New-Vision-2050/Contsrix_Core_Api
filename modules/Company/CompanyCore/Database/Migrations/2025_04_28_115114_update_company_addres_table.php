1e4b94059cf4b79a7302738a14d993e6f528ce25
AmrSaleh2234 <amrsaleh1001@gmail.com>
2025-04-28 14:59:44 +0300

    update address id

--- modules/Company/CompanyCore/Database/Migrations/2025_04_28_115114_update_company_addres_table.php
+++ modules/Company/CompanyCore/Database/Migrations/2025_04_28_115114_update_company_addres_table.php
@@ -4,16 +4,12 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 
-return new class extends Migration
-{
+return new class extends Migration {
     public function up()
     {
-        Schema::table('management_hierarchy_details', function (Blueprint $table) {
-            $table->dropColumn("id");
-            $table->id()->first();
+        Schema::table('company_address', function (Blueprint $table) {
             $table->dropColumn("management_hierarchy_id");
             $table->unsignedBigInteger("management_hierarchy_id")->nullable()->index();
-
         });
     }
 };
