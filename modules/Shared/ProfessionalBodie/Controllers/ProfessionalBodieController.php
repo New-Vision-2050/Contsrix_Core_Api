dd60f2f74de70fed616ea7cee5c70e5528066237
Abdelrahman El-Desoky <dev.deesoky@gmail.com>
2025-04-28 03:01:56 +0300

    make filter

--- modules/Shared/ProfessionalBodie/Controllers/ProfessionalBodieController.php
+++ modules/Shared/ProfessionalBodie/Controllers/ProfessionalBodieController.php
@@ -16,6 +16,7 @@ use Modules\Shared\ProfessionalBodie\Requests\GetProfessionalBodieListRequest;
 use Modules\Shared\ProfessionalBodie\Requests\GetProfessionalBodieRequest;
 use Modules\Shared\ProfessionalBodie\Requests\UpdateProfessionalBodieRequest;
 use Modules\Shared\ProfessionalBodie\Services\ProfessionalBodieCRUDService;
+use Modules\User\Repositories\UserRepository;
 use Ramsey\Uuid\Uuid;
 
 class ProfessionalBodieController extends Controller
@@ -24,12 +25,24 @@ class ProfessionalBodieController extends Controller
         private ProfessionalBodieCRUDService $professionalBodieService,
         private UpdateProfessionalBodieHandler $updateProfessionalBodieHandler,
         private DeleteProfessionalBodieHandler $deleteProfessionalBodieHandler,
+        private UserRepository $userRepository
     ) {
     }
 
-    public function index(GetProfessionalBodieListRequest $request): JsonResponse
+    public function index(GetProfessionalBodieListRequest $request)//: JsonResponse
     {
+        $userId = Uuid::fromString($request->route('id'));
+        $user = $this->userRepository->getUser($userId);
+
+       $getCode = $this->professionalBodieService->getCode(
+        Uuid::fromString($user->company_id),
+        Uuid::fromString($user->global_company_user_id),
+        );
+
+
+
         $list = $this->professionalBodieService->list(
+            $getCode,
             (int) $request->get('page', 1),
             (int) $request->get('per_page', 10)
         );
