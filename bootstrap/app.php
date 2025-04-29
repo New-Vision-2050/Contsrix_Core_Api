08cc1caeb619c277c3425f8e806212b06612b3cf
mabou7agar <13303169+mabou7agar@users.noreply.github.com>
2025-04-29 10:55:01 +0300

    Add Sentry

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -6,6 +6,7 @@ use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Validation\ValidationException;
 use Illuminate\Auth\AuthenticationException;
 use Illuminate\Auth\Access\AuthorizationException;
+use Sentry\Laravel\Integration;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use App\Exceptions\Handler;
 return Application::configure(basePath: dirname(__DIR__))
@@ -27,6 +28,7 @@ return Application::configure(basePath: dirname(__DIR__))
 
     })
     ->withExceptions(function (Exceptions $exceptions) {
+        Integration::handles($exceptions);
         $exceptions->render(function (Throwable $e, $request) {
             return Handler::handle($e); // Use the custom handler
         });
