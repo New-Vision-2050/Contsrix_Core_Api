5394f6e7b8553c8ae34da3dfee866fa089ae3fe4
AmrSaleh2234 <amrsaleh1001@gmail.com>
2025-05-04 12:47:32 +0300

    move with relation into repositery

--- modules/Company/CompanyCore/Repositories/CompanyRepository.php
+++ modules/Company/CompanyCore/Repositories/CompanyRepository.php
@@ -21,6 +21,7 @@ use Carbon\Carbon;
  */
 class CompanyRepository extends BaseRepository
 {
+    private $relations= ['country', 'companyType', 'companyField', 'companyRegistrationType', 'generalManager', "mainBranch", "companyLegalData.media", "companyOfficialDocuments.media", "companyOfficialDocuments.activityLogs", "companyAddress","owner"];
     use PreDeclareComapnyAndBranchDependOnReqeuest;
     public function __construct(Company $model)
     {
@@ -168,20 +169,20 @@ class CompanyRepository extends BaseRepository
 
     public function get()
     {
-        return $this->model->get();
+        return $this->model->with($this->relations)->get();
     }
 
     public function getCurrentCompany() : Company
     {
        [$company , $branch] =$this->declareCompanyAndBranchUsingRequest();
-        return $this->model->where("id",$company->id)->with(["companyAddress"=>function ($query)use ($branch) {
+        return $this->model->where("id",$company->id)->with(array_merge($this->relations ,["companyAddress"=>function ($query)use ($branch) {
             $query->where("management_hierarchy_id",$branch->id);
         },"companyLegalData"=>function ($query)use ($branch) {
             $query->where("management_hierarchy_id",$branch->id);
 
         },"companyOfficialDocuments"=>function ($query)use ($branch) {
             $query->where("management_hierarchy_id",$branch->id);}
-        ])->first();
+        ]))->first();
     }
 
     public function getAllWithRelations(array $relations = []): Collection
@@ -198,6 +199,6 @@ class CompanyRepository extends BaseRepository
 
     public function getLastCreatedCompany(): ?Company
     {
-        return $this->model->latest('created_at')->first();
+        return $this->model->latest('created_at')->with($this->relations)->first();
     }
 }
