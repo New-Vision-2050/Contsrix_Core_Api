2bf61105cb2008895a9b32659f59c73d4dd24023
Abdelrahman El-Desoky <dev.deesoky@gmail.com>
2025-05-05 17:09:12 +0300

    add companyType in UpdateOfficialCompanyDat

--- modules/Company/CompanyCore/Requests/CompanyProfile/UpdateOfficialCompanyData.php
+++ modules/Company/CompanyCore/Requests/CompanyProfile/UpdateOfficialCompanyData.php
@@ -20,6 +20,7 @@ class UpdateOfficialCompanyData extends FormRequest
             'email' => 'required|email|string|unique:companies,email,' . Uuid::fromString(tenant("id")),
             'phone' => 'required|string',
             'branch_name' => 'required|string',
+            'company_type' => 'required|string',
         ];
     }
 
@@ -33,6 +34,7 @@ class UpdateOfficialCompanyData extends FormRequest
             email: $this->get('email'),
             phone: $this->get('phone'),
             branchName: $this->get('branch_name'),
+            companyType: $this->get('company_type'),
         );
     }
 }
