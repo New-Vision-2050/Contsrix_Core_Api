414364713b4a01ead78dc94e187c134d48986a91
Momtaz Nussair <momtaz@osarh.com>
2025-05-07 01:36:04 +0300

    Update SubEntityFactory

--- modules/SubEntity/Database/factories/SubEntityFactory.php
+++ modules/SubEntity/Database/factories/SubEntityFactory.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Modules\SubEntity\Database\factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Modules\Program\Models\Program;
 use Modules\SubEntity\Models\SubEntity;
 
 /** @extends Factory<SubEntity> */
@@ -15,7 +16,23 @@ class SubEntityFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => $this->faker->name(),
+            'id' => $this->faker->uuid(),
+            'super_entity' => $this->faker->randomElement(['User', 'Company', 'Department']),
+            'name' => $this->faker->unique()->word(),
+            'icon' => $this->faker->numberBetween(0, 255),
+            'main_program_id' => Program::first(), // TODO
+            'is_active' => $this->faker->boolean(70), // 80% chance of being active
+            'is_registrable' => $this->faker->boolean(75), // 30% chance of being registrable
+            'default_attributes' => json_encode([
+                'name',
+                'email',
+                'phone',
+            ]),
+            'optional_attributes' => $this->faker->optional()->passthrough(json_encode([
+                "company_id",
+                "is_owner",
+                "management_hierarchy_id"
+            ])),
         ];
     }
 }
