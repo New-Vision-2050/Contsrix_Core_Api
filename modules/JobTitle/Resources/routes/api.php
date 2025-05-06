23bbffb6950a773752684877acf38807e15d6d42
AmrSaleh2234 <amrsaleh1001@gmail.com>
2025-05-06 13:47:57 +0300

    list data in job title

--- modules/JobTitle/Resources/routes/api.php
+++ modules/JobTitle/Resources/routes/api.php
@@ -5,6 +5,7 @@ use Modules\JobTitle\Controllers\JobTitleController;
 
 Route::group(['middleware' => ['auth:api',\Stancl\Tenancy\Middleware\InitializeTenancyByRequestData::class]], function () {
     Route::get('/', [JobTitleController::class, 'index']);
+    Route::get('/list', [JobTitleController::class, 'listSimple']);
     Route::post('/', [JobTitleController::class, 'store']);
     Route::get('/{id}', [JobTitleController::class, 'show']);
     Route::put('/{id}', [JobTitleController::class, 'update']);
