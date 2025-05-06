03b09a88192539ac5076ec5f4bbaaaa28f3164da
AmrSaleh2234 <amrsaleh1001@gmail.com>
2025-05-06 13:06:43 +0300

    crud operation

--- modules/JobTitle/DTO/CreateJobTitleDTO.php
+++ modules/JobTitle/DTO/CreateJobTitleDTO.php
@@ -5,11 +5,14 @@ declare(strict_types=1);
 namespace Modules\JobTitle\DTO;
 
 use Ramsey\Uuid\UuidInterface;
+use Ramsey\Uuid\Uuid;
 
 class CreateJobTitleDTO
 {
     public function __construct(
         public string $name,
+        public ?UuidInterface $job_type_id = null,
+        public ?string $description = null,
     ) {
     }
 
@@ -17,6 +20,10 @@ class CreateJobTitleDTO
     {
         return [
             'name' => $this->name,
+            'job_type_id' => $this->job_type_id,
+            'description' => $this->description,
+            'company_id' => tenant("id"),
+            "status"=>1
         ];
     }
 }
