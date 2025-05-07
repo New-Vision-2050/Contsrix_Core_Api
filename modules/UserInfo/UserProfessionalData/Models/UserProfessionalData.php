ec0168a438dc640e662d374977b9fc2cd09e696f
AmrSaleh2234 <amrsaleh1001@gmail.com>
2025-05-07 10:11:16 +0300

    make relation in user profissional belongs to user

--- modules/UserInfo/UserProfessionalData/Models/UserProfessionalData.php
+++ modules/UserInfo/UserProfessionalData/Models/UserProfessionalData.php
@@ -13,6 +13,8 @@ use BasePackage\Shared\Traits\BaseFilterable;
 use Modules\Country\Models\Country;
 use Modules\JobTitle\Models\JobTitle;
 use Modules\Shared\JobType\Models\JobType;
+use Stancl\Tenancy\Database\Concerns\BelongsToTenant;
+use Modules\User\Models\User;
 
 //use BasePackage\Shared\Traits\HasTranslations;
 
@@ -23,6 +25,7 @@ class UserProfessionalData extends Model
     use BaseFilterable;
     //use HasTranslations;
     //use SoftDeletes;
+    use BelongsToTenant;//we can use belongs to primary model user or belongs to tenant because have company id
 
     //public array $translatable = [];
     protected $table = 'user_professional_datas';
@@ -80,5 +83,10 @@ class UserProfessionalData extends Model
         return $this->belongsTo(JobTitle::class);
     }
 
+    public function user()//TODO under Testing not used up till now
+    {
+        return $this->belongsTo(User::class, 'global_id', 'global_company_user_id')
+            ->where('users.company_id', '=', $this->company_id);
+    }
 
 }
