9f6fe7c133e1eea62eba1e0c6af9f3f72f7fb9ec
Momtaz Nussair <momtaz@osarh.com>
2025-05-11 02:03:37 +0300

    [SCRUM-57] Update Search by name sub entity filter

--- modules/SubEntity/Filters/SubEntityFilter.php
+++ modules/SubEntity/Filters/SubEntityFilter.php
@@ -12,6 +12,6 @@ class SubEntityFilter extends SearchModelFilter
 
         public function name($name)
         {
-            return $this->where('name', $name);
+            return $this->whereLike('name', "%$name%");
         }
 }
