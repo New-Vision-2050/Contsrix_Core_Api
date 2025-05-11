6ce63c5eed4551e94d29d484b068f9e089415021
Abdelrahman El-Desoky <dev.deesoky@gmail.com>
2025-05-11 14:28:47 +0300

    return new class extends Migration

---
+++ modules/SubEntity/Database/Migrations/2025_05_11_010745_add_origin_super_entity_name_to_sub_entities_table.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::table('sub_entities', function (Blueprint $table) {
+            $table->string('origin_super_entity')->nullable()->change();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::table('sub_entities', function (Blueprint $table) {
+            $table->string('origin_super_entity')->nullable(false)->change();
+        });
+    }
+};
