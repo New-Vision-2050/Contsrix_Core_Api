7aa815b887c3b2bf8030427bddaed10db1121743
AmrSaleh2234 <amrsaleh1001@gmail.com>
2025-05-26 12:17:19 +0300

    omit assign to branch

--- modules/CompanyUser/Repositories/CompanyUserRepository.php
+++ modules/CompanyUser/Repositories/CompanyUserRepository.php
@@ -190,9 +190,9 @@ class CompanyUserRepository extends BaseRepository
             $companyUser = $companyUser->fresh();//get updated data for company user
             $user = $this->userRepository->model->withTrashed()->withoutTenancy()->where(["global_company_user_id" => $companyUser->global_id, "company_id" => $companyRole['company_id']])->first();
             $mainBranchId = ManagementHierarchy::query()->where("company_id", $companyRole['company_id'])->where("parent_id", null)->first()->id;
-            $mainManagement = ManagementHierarchy::query()->where("company_id", $companyRole['company_id'])->where("parent_id", $mainBranchId)->first();
+            $mainManagement = ManagementHierarchy::query()->where("company_id", $companyRole['company_id'])->where("parent_id", $mainBranchId)->where("type","management")->first();
             if ($branches != null && CompanyUserRole::EMPLOYEE->value == $companyRole['role']) {
-                $mainManagement = ManagementHierarchy::query()->where("company_id", $companyRole['company_id'])->where("parent_id", $branches[0])->first();
+                $mainManagement = ManagementHierarchy::query()->where("company_id", $companyRole['company_id'])->where("parent_id", $branches[0])->where("type","management")->first();
 
             }
             if (!$user) {//must create user if use api createCompanyUser because validation prevent replicate
