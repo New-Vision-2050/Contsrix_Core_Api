367827ecb394e23cda5d5b3b8348d1e98da9d076
AmrSaleh2234 <amrsaleh1001@gmail.com>
2025-05-26 11:43:45 +0300

    append branches

--- modules/CompanyUser/Services/CompanyUserValidationService.php
+++ modules/CompanyUser/Services/CompanyUserValidationService.php
@@ -16,9 +16,10 @@ class CompanyUserValidationService
 {
 
     private $errors = [];
+
     public function __construct(
         private CompanyUserRepository $repository,
-        private UserRepository $userRepository,
+        private UserRepository        $userRepository,
     )
     {
 
@@ -26,15 +27,15 @@ class CompanyUserValidationService
 
     public function validateName()
     {
-        if(!preg_match("/\p{Arabic}/u", request()->name) ||count(explode(" ", request()->name)) <3){
-            $this->errors[] =   [
+        if (!preg_match("/\p{Arabic}/u", request()->name) || count(explode(" ", request()->name)) < 3) {
+            $this->errors[] = [
                 'sentence' => __("validation.user-name"),
                 'sub_title' => '',
                 'status' => 0,
                 'validate' => 'required'
             ];
-        }else{
-            $this->errors[] =   [
+        } else {
+            $this->errors[] = [
                 'sentence' => __("validation.user-name"),
                 'sub_title' => '',
                 'status' => 1,
@@ -46,22 +47,27 @@ class CompanyUserValidationService
 
     public function validateEmail()
     {
-        if($user = $this->repository->findByEmail(request()->email)) {
-            $userInCompany = $this->userRepository->findOneBy(["email"=>request()->email,"company_id"=>tenant("id")]);
+        if ($user = $this->repository->findByEmail(request()->email)) {
+            $userInCompany = $this->userRepository->findOneBy(["email" => request()->email, "company_id" => tenant("id")]);
+            $role = request()->has("role") ? request()->role : 1;
+            $companyUserCompany = $this->userRepository->getUserByGlobalIdWithBranches($userInCompany->global_company_user_id, $role);
+
             $this->errors[] = [
-                'sentence' => __("validation.user-email-error",["name"=>$user->name]),
+                'sentence' => __("validation.user-email-error", ["name" => $user->name]),
                 'sub_title' => 'email',
                 'status' => 0,
-                "status_in_company"=>$userInCompany == null?0:1,
+                "status_in_company" => $userInCompany == null ? 0 : 1,
+                "branches" => $companyUserCompany?->managementHierarchy?$companyUserCompany?->managementHierarchy:[],
                 'validate' => 'required',
-                'id' =>$user->id
+                'id' => $user->id
             ];
-        }else{
+        } else {
             $this->errors[] = [
                 'sentence' => __("validation.user-email-success"),
                 'sub_title' => 'email',
                 'status' => 1,
-                "status_in_company"=>0,
+                "branches" => [],
+                "status_in_company" => 0,
                 'validate' => 'required'
             ];
         }
@@ -70,24 +76,22 @@ class CompanyUserValidationService
 
     public function validatePhone()
     {
-        $validator = Validator::make(request()->all(), ['phone'=>'required|phone']);
-        if($this->repository->findByPhone(request()->phone)) {
+        $validator = Validator::make(request()->all(), ['phone' => 'required|phone']);
+        if ($this->repository->findByPhone(request()->phone)) {
             $this->errors[] = [
                 'sentence' => __("validation.user-phone-error"),
                 'sub_title' => 'phone',
                 'status' => 0,
                 'validate' => 'required'
             ];
-        }
-        else if($validator->fails()){
+        } else if ($validator->fails()) {
             $this->errors[] = [
                 'sentence' => $validator->errors()->messages()["phone"][0],
                 'sub_title' => 'phone',
                 'status' => 0,
                 'validate' => 'required'
             ];
-        }
-        else{
+        } else {
             $this->errors[] = [
                 'sentence' => __("validation.user-phone-success"),
                 'sub_title' => 'phone',
