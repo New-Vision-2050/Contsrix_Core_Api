33000134645274a83c86dd52a7fccf337bf8cff6
AbdelrahmanEl-Desoky <abdelrhman@visualinnovate.com>
2025-05-27 10:11:19 +0300

    add CompanyLegalDataPresenter registration_type

--- modules/Company/CompanyCore/Presenters/CompanyLegalDataPresenter.php
+++ modules/Company/CompanyCore/Presenters/CompanyLegalDataPresenter.php
@@ -7,6 +7,7 @@ namespace Modules\Company\CompanyCore\Presenters;
 use Modules\Company\CompanyCore\Models\Company;
 use BasePackage\Shared\Presenters\AbstractPresenter;
 use Modules\Company\CompanyCore\Models\CompanyLegalData;
+use Modules\Company\CompanyRegistrationType\Presenters\CompanyRegistrationTypePresenter;
 use Modules\Shared\Media\Presenters\MediaPresenter;
 
 class CompanyLegalDataPresenter extends AbstractPresenter
@@ -23,7 +24,7 @@ class CompanyLegalDataPresenter extends AbstractPresenter
         return [
             'id' => $this->company->id,
             'registration_number' => $this->company->registration_number,
-            'registration_type' => $this->company->registrationType->name,
+            'registration_type' => $this->company->registrationType? (new CompanyRegistrationTypePresenter($this->company->registrationType))->getData() : null,
             'registration_type_id' => $this->company->registration_type_id,
             "start_date"=>$this->company->start_date,
             "end_date"=>$this->company->end_date,
