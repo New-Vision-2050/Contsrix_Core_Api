459f8e9b91b028ec09b88e93e842fce5550d42dc
AmrSaleh2234 <amrsaleh1001@gmail.com>
2025-05-31 03:48:38 +0300

    add in seeder branch_id "

--- routes/web.php
+++ routes/web.php
@@ -3,16 +3,5 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    $details = \Modules\Company\ManagementHierarchy\Models\ManagementHierarchyDetail::query()->withoutParentModel()->whereNull("branch_id")->with("managementHierarchy")->get();
-    foreach ($details as $detail) {
-
-//        $branch = \Modules\Company\ManagementHierarchy\Models\ManagementHierarchy::query()->withoutTenancy()->where("company_id", $detail->managementHierarchy->company_id)
-//            ->where("type", "branch")->whereNull("parent_id")->first();
-        $managemant = $detail->managementHierarchy;
-        while ($managemant->type !="branch")
-        {
-
-        }
-        $detail->update(["branch_id" => $branch->id]);
-
-    }});
+    return view('welcome');
+});
