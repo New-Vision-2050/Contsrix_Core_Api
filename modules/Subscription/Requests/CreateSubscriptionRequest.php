7b70b84a4c6728b565cd1048d5936eafdb43b34f
Momtaz Nussair <momtaz@osarh.com>
2025-06-15 22:26:50 +0300

    Create Subscription Module

---
+++ modules/Subscription/Requests/CreateSubscriptionRequest.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Modules\Subscription\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Ramsey\Uuid\Uuid;
+use Modules\Subscription\DTO\CreateSubscriptionDTO;
+
+class CreateSubscriptionRequest extends FormRequest
+{
+    public function rules(): array
+    {
+        return [
+            'name' => 'required|string',
+        ];
+    }
+
+    public function createCreateSubscriptionDTO(): CreateSubscriptionDTO
+    {
+        return new CreateSubscriptionDTO(
+            name: $this->get('name'),
+        );
+    }
+}
