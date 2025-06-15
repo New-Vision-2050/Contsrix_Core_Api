197ddcd247e31ed7b9e813605d733230200fbd39
AmrSaleh2234 <amrsaleh1001@gmail.com>
2025-06-15 10:43:45 +0300

    omit roles and permissions from the top

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -57,7 +57,6 @@ class DatabaseSeeder extends Seeder
     {
         $this->call(CurrencySeederTable::class);
 //        $this->call(UniversitiesTableSeeder::class);
-        $this->call(RolesAndPermissionsSeeder::class);
         $this->call(TimeZoneSeederTableSeeder::class);
         $this->call(LanguagesTableSeeder::class);
         $this->call(JobTitleModulesSeederTableSeeder::class);
