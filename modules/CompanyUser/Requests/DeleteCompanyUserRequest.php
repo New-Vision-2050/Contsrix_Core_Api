8d022a33400e6dd515efd6e674cd2648f305304b
AmrSaleh2234 <amrsaleh1001@gmail.com>
2025-06-24 11:55:59 +0300

    validation and skip queue

--- modules/CompanyUser/Requests/DeleteCompanyUserRequest.php
+++ modules/CompanyUser/Requests/DeleteCompanyUserRequest.php
@@ -5,12 +5,15 @@ declare(strict_types=1);
 namespace Modules\CompanyUser\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Modules\CompanyUser\Rules\CanDeleteCompanyUserRule;
 use Ramsey\Uuid\Uuid;
 
 class DeleteCompanyUserRequest extends FormRequest
 {
     public function rules(): array
     {
-        return [];
+        return [
+            'id' => ['required', 'uuid', 'exists:company_users,id', new CanDeleteCompanyUserRule()],
+        ];
     }
 }
