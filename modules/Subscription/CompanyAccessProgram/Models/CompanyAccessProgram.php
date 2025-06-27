3e873a5a70c57c26836fa304906c6266ecb3bce3
Momtaz Nussair <momtaz@osarh.com>
2025-06-27 19:02:15 +0300

    Add CompanyAccessProgram Relations

--- modules/Subscription/CompanyAccessProgram/Models/CompanyAccessProgram.php
+++ modules/Subscription/CompanyAccessProgram/Models/CompanyAccessProgram.php
@@ -4,25 +4,24 @@ declare(strict_types=1);
 
 namespace Modules\Subscription\CompanyAccessProgram\Models;
 
-use BasePackage\Shared\Traits\UuidTrait;
+use Modules\Country\Models\Country;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Modules\Subscription\CompanyAccessProgram\Database\factories\CompanyAccessProgramFactory;
+use BasePackage\Shared\Traits\UuidTrait;
 use BasePackage\Shared\Traits\BaseFilterable;
-//use BasePackage\Shared\Traits\HasTranslations;
+use Modules\Subscription\Module\Models\Module;
+use Modules\Company\CompanyType\Models\CompanyType;
+use Modules\Company\CompanyField\Models\CompanyField;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Modules\Subscription\CompanyAccessProgram\Database\factories\CompanyAccessProgramFactory;
 
 class CompanyAccessProgram extends Model
 {
     use HasFactory;
     use UuidTrait;
     use BaseFilterable;
-    //use HasTranslations;
-    //use SoftDeletes;
-
-    //public array $translatable = [];
 
     public $incrementing = false;
-
     protected $keyType = 'string';
 
     protected $fillable = [
@@ -36,5 +35,45 @@ class CompanyAccessProgram extends Model
     protected static function newFactory(): CompanyAccessProgramFactory
     {
         return CompanyAccessProgramFactory::new();
+    }
+
+    public function modules(): BelongsToMany
+    {
+        return $this->belongsToMany(
+            Module::class,
+            'company_access_program_module',
+            'company_access_program_id',
+            'module_id'
+        );
+    }
+
+    public function companyFields(): BelongsToMany
+    {
+        return $this->belongsToMany(
+            CompanyField::class,
+            'company_access_program_field',
+            'company_access_program_id',
+            'company_field_id'
+        );
+    }
+
+    public function companyTypes(): BelongsToMany
+    {
+        return $this->belongsToMany(
+            CompanyType::class,
+            'company_access_program_type',
+            'company_access_program_id',
+            'company_type_id'
+        );
+    }
+
+    public function countries(): BelongsToMany
+    {
+        return $this->belongsToMany(
+            Country::class,
+            'company_access_program_country',
+            'company_access_program_id',
+            'country_id'
+        );
     }
 }
