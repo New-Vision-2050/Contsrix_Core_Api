31408691158879009d389867b2bc363f65ead924
Momtaz Nussair <momtaz@osarh.com>
2025-06-27 13:32:04 +0300

    Remove boilerplate subscription module code

--- modules/Subscription/Database/Migrations/2025_06_15_174431_create_package_features_table.php
+++ modules/Subscription/Database/Migrations/2025_06_15_174431_create_package_features_table.php
@@ -7,21 +7,22 @@ use Illuminate\Database\Schema\Blueprint;
 return new class extends Migration {
     public function up()
     {
-        Schema::create('package_feature_settings', function (Blueprint $table) {
-            $table->uuid('package_id');
-            $table->uuid('feature_id');
+        Schema::create('package_features', function (Blueprint $table) {
 
+            $table->uuid('id')->primary();
+            $table->json('name');
+            $table->string('slug');
             $table->boolean('is_enabled')->default(true);
-
             $table->unsignedInteger('limit')->nullable(); // optional usage limit
             $table->string('limit_type')->nullable();     // e.g., per_day, per_month, per_user, gb, minutes, per_package
 
-            $table->timestamps();
+            $table->uuid('module_id');
+            $table->uuid('package_id');
 
             $table->foreign('package_id')->references('id')->on('packages')->onDelete('cascade');
-            $table->foreign('feature_id')->references('id')->on('features')->onDelete('cascade');
+            $table->foreign('module_id')->references('id')->on('modules')->cascadeOnDelete();
 
-            $table->unique(['package_id', 'feature_id']);
+            $table->timestamps();
         });
     }
 
