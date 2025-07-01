4e7ca82fbbb0e6628902aa2567f66cc5495ccd72
Momtaz Nussair <momtaz@osarh.com>
2025-07-01 05:04:55 +0300

    Create Package Module and Create Package request

---
+++ modules/Subscription/Package/Services/PackageCRUDService.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Modules\Subscription\Package\Services;
+
+use Illuminate\Support\Collection;
+use Modules\Subscription\Package\DTO\CreatePackageDTO;
+use Modules\Subscription\Package\Models\Package;
+use Modules\Subscription\Package\Repositories\PackageRepository;
+use Ramsey\Uuid\UuidInterface;
+
+class PackageCRUDService
+{
+    public function __construct(
+        private PackageRepository $repository,
+    ) {
+    }
+
+    public function create(CreatePackageDTO $createPackageDTO): Package
+    {
+         return $this->repository->createPackage($createPackageDTO->toArray());
+    }
+
+    public function list(int $page = 1, int $perPage = 10): array
+    {
+        return $this->repository->paginated(
+            page: $page,
+            perPage: $perPage,
+        );
+    }
+
+    public function get(UuidInterface $id): Package
+    {
+        return $this->repository->getPackage(
+            id: $id,
+        );
+    }
+}
