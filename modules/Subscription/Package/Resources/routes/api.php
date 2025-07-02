a334331c86bd19641497ecfa9096588f5b397e9f
Momtaz Nussair <momtaz@osarh.com>
2025-07-02 20:43:53 +0300

    Create Attach Package Features Endpoint

--- modules/Subscription/Package/Resources/routes/api.php
+++ modules/Subscription/Package/Resources/routes/api.php
@@ -6,6 +6,7 @@ use Modules\Subscription\Package\Controllers\PackageController;
 Route::group(['middleware' => ['auth:api']], function () {
     Route::get('/', [PackageController::class, 'index']);
     Route::post('/', [PackageController::class, 'store']);
+    Route::post('/attach-features', [PackageController::class, 'attachFeatures']);
     Route::get('/{id}', [PackageController::class, 'show']);
     Route::put('/{id}', [PackageController::class, 'update']);
     Route::delete('/{id}', [PackageController::class, 'delete']);
--- modules/Subscription/Package/Services/PackageCRUDService.php
+++ modules/Subscription/Package/Services/PackageCRUDService.php
@@ -4,11 +4,12 @@ declare(strict_types=1);
 
 namespace Modules\Subscription\Package\Services;
 
-use Illuminate\Support\Collection;
-use Modules\Subscription\Package\DTO\CreatePackageDTO;
-use Modules\Subscription\Package\Models\Package;
-use Modules\Subscription\Package\Repositories\PackageRepository;
+use Ramsey\Uuid\Uuid;
 use Ramsey\Uuid\UuidInterface;
+use Illuminate\Support\Collection;
+use Modules\Subscription\Package\Models\Package;
+use Modules\Subscription\Package\DTO\CreatePackageDTO;
+use Modules\Subscription\Package\Repositories\PackageRepository;
 
 class PackageCRUDService
 {
@@ -19,7 +20,7 @@ class PackageCRUDService
 
     public function create(CreatePackageDTO $createPackageDTO): Package
     {
-         return $this->repository->createPackage($createPackageDTO->toArray());
+        return $this->repository->createPackage($createPackageDTO->toArray());
     }
 
     public function list(int $page = 1, int $perPage = 10): array
@@ -35,5 +36,10 @@ class PackageCRUDService
         return $this->repository->getPackage(
             id: $id,
         );
+    }
+
+    public function attachFeatures(string $packageId, array $features): void
+    {
+        $this->repository->upsertFeatures($packageId, $features);
     }
 }
