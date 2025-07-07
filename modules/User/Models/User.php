6d56f9245f5d44ccac421040fbbc4662c5607b1d
Abdelrahman El-Desoky <dev.deesoky@gmail.com>
2025-07-07 14:14:15 +0300

    feature/add_leave_managment_system

--- modules/User/Models/User.php
+++ modules/User/Models/User.php
@@ -9,7 +9,6 @@ use App\Casts\UuidCast;
 use Modules\CompanyUser\Models\ClientDetail;
 use Modules\Setting\Models\LoginWay;
 use App\Traits\CustomBelongsToTenant;
-use Modules\UserInfo\UserProfessionalData\Models\UserProfessionalData;
 use Spatie\Permission\Traits\HasRoles;
 use Tymon\JWTAuth\Contracts\JWTSubject;
 use BasePackage\Shared\Traits\UuidTrait;
