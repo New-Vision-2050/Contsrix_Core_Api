5f7ce200f8b46e9f11bfc736578a53ff46291345
Abdelrahman El-Desoky <dev.deesoky@gmail.com>
2025-07-08 16:16:19 +0300

    feature/add_leave_managment_system stage

--- modules/Attendance/Requests/UpdateAttendanceConstraintRequest.php
+++ modules/Attendance/Requests/UpdateAttendanceConstraintRequest.php
@@ -14,10 +14,10 @@ class UpdateAttendanceConstraintRequest extends FormRequest
     /**
      * Determine if the user is authorized to make this request.
      */
-    public function authorize(): bool
-    {
-        return $this->user()->can('update_attendance_constraints');
-    }
+    // public function authorize(): bool
+    // {
+    //     return $this->user()->can('update_attendance_constraints');
+    // }
 
     /**
      * Get the validation rules that apply to the request.
