f52df57b7d4888fed422447ef51b3c120e3aa9f2
AmrSaleh2234 <amrsaleh1001@gmail.com>
2025-07-08 14:45:30 +0300

    fix filter

--- modules/Company/CompanyField/Filters/CompanyFieldFilter.php
+++ modules/Company/CompanyField/Filters/CompanyFieldFilter.php
@@ -15,7 +15,7 @@ class CompanyFieldFilter extends SearchModelFilter
         return $this->where('name', 'LIKE', "%{$name}%");
     }
 
-    public function companyAccessProgramId()
+    public function companyAccessProgram()
     {
         return $this->whereHas('companyAccessProgram', function ($query) {
             $query->where('company_access_programs.id', request('company_access_program_id'));
