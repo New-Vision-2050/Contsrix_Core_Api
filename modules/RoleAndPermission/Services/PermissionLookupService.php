f0558f7d3c15c0970a0dd4cd862719bc78394970
AmrSaleh2234 <amrsaleh1001@gmail.com>
2025-07-14 10:53:07 +0300

    apply filter

--- modules/RoleAndPermission/Services/PermissionLookupService.php
+++ modules/RoleAndPermission/Services/PermissionLookupService.php
@@ -6,14 +6,23 @@ namespace Modules\RoleAndPermission\Services;
 
 use Illuminate\Support\Collection;
 use Modules\Company\CompanyCore\Repositories\CompanyRepository;
+use Modules\RoleAndPermission\Repositories\PermissionRepository;
+use Modules\Subscription\CompanyAccessProgram\Repositories\CompanyAccessProgramRepository;
+use Modules\Subscription\Package\Models\Package;
+use Modules\Subscription\Package\Repositories\PackageRepository;
 use Ramsey\Uuid\Uuid;
 
 class PermissionLookupService
 {
     public function __construct(
         private CompanyRepository $companyRepository,
-    ) {
+        private PackageRepository $packageRepository,
+        private CompanyAccessProgramRepository $companyAccessProgramRepository,
+        private PermissionRepository $permissionRepository
+    )
+    {
     }
+
     public function getPermissionsForCompany(): Collection
     {
         $company = $this->companyRepository->getCompany(UUid::fromString(tenant("id")));
@@ -31,5 +40,14 @@ class PermissionLookupService
             ->pluck('permissions')
             ->flatten()
             ->unique('id');
+    }
+
+    public function getPermissionsForPackage($packageId): Collection
+    {
+        $package = $this->packageRepository->getPackage(Uuid::fromString($packageId));
+        $companyAccessPrograms = $this->companyAccessProgramRepository->getCompanyAccessProgram(Uuid::fromString($package->company_access_program_id));
+        $subEntities = $companyAccessPrograms->subEntities()->pluck("sub_entity_id")->toArray();
+
+        return $this->permissionRepository->getPermissionsBySubEntities($subEntities);
     }
 }
