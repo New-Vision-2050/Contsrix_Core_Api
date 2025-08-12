4dd208014005879ee94968a89a3c6ac8a4e59b4e
AmrSaleh2234 <amrsaleh1001@gmail.com>
2025-08-12 16:10:41 +0300

    apply permissions in  user profile

--- modules/UserInfo/UserProfessionalData/Resources/routes/api.php
+++ modules/UserInfo/UserProfessionalData/Resources/routes/api.php
@@ -5,9 +5,9 @@ use Modules\UserInfo\UserProfessionalData\Controllers\UserProfessionalDataContro
 use Modules\RoleAndPermission\Enums\Permission;
 
 Route::group(['middleware' => ['auth:api',\Stancl\Tenancy\Middleware\InitializeTenancyByRequestData::class]], function () {
-    Route::get('user/{id}', [UserProfessionalDataController::class, 'index'])->permission(Permission::PROFILE_WORK_LICENSE_VIEW());
-    Route::post('/', [UserProfessionalDataController::class, 'store'])->permission(Permission::PROFILE_WORK_LICENSE_CREATE());
-    Route::get('/{id}', [UserProfessionalDataController::class, 'show'])->permission(Permission::PROFILE_WORK_LICENSE_VIEW());
-    Route::put('/{id}', [UserProfessionalDataController::class, 'update'])->permission(Permission::PROFILE_WORK_LICENSE_UPDATE());
-    Route::delete('/{id}', [UserProfessionalDataController::class, 'delete'])->permission(Permission::PROFILE_WORK_LICENSE_UPDATE());
+    Route::get('user/{id}', [UserProfessionalDataController::class, 'index']);
+    Route::post('/', [UserProfessionalDataController::class, 'store']);
+    Route::get('/{id}', [UserProfessionalDataController::class, 'show'])->permission(Permission::PROFILE_CONTRACT_WORK_VIEW());
+    Route::put('/{id}', [UserProfessionalDataController::class, 'update'])->permission(Permission::PROFILE_CONTRACT_WORK_VIEW());
+    Route::delete('/{id}', [UserProfessionalDataController::class, 'delete'])->permission(Permission::PROFILE_CONTRACT_WORK_VIEW());
 });
