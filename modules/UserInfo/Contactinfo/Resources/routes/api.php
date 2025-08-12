8f6ee45f05b10f40efd1da606b89870f578f629c
AmrSaleh2234 <amrsaleh1001@gmail.com>
2025-08-12 15:52:22 +0300

    apply permissions in  user profile

--- modules/UserInfo/Contactinfo/Resources/routes/api.php
+++ modules/UserInfo/Contactinfo/Resources/routes/api.php
@@ -5,7 +5,7 @@ use Modules\UserInfo\Contactinfo\Controllers\ContactinfoController;
 use Modules\RoleAndPermission\Enums\Permission;
 
 Route::group(['middleware' => ['auth:api',\Stancl\Tenancy\Middleware\InitializeTenancyByRequestData::class]], function () {
-    Route::get('/{id}', [ContactinfoController::class, 'show'])->permission(Permission::PROFILE_CONTACT_INFO_VIEW());
+    Route::get('/{id}', [ContactinfoController::class, 'show'])->permission(Permission::PROFILE_CONTACT_INFO_VIEW(),Permission::PROFILE_ADDRESS_INFO_VIEW());
     Route::put('/{id}', [ContactinfoController::class, 'update'])->permission(Permission::PROFILE_CONTACT_INFO_UPDATE());
     Route::put('address/{id}', [ContactinfoController::class, 'updateAddress'])->permission(Permission::PROFILE_ADDRESS_INFO_UPDATE());
 
