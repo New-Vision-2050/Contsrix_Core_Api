75c69df288f7b3bf33e1eb31fcbae46eebae7fd9
Abdelrahman El-Desoky <dev.deesoky@gmail.com>
2025-08-13 14:55:30 +0300

    fix cerate generateAttendanceUsers

--- modules/CompanyUser/Handlers/AssignRoleCompanyUserHandler.php
+++ modules/CompanyUser/Handlers/AssignRoleCompanyUserHandler.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace Modules\CompanyUser\Handlers;
 
 use App\Exceptions\CustomException;
-use Modules\Attendance\Services\AutoAttendanceService;
 use Modules\Company\CompanyCore\Notifications\SendDomainForUser;
 use Modules\CompanyUser\Commands\AssignRoleCompanyUserCommand;
 use Modules\CompanyUser\Commands\UpdateCompanyUserCommand;
@@ -20,8 +19,7 @@ class AssignRoleCompanyUserHandler
     public function __construct(
         private CompanyUserRepository $repository,
         private UserRepository $userRepository,
-        private CompanyUserCRUDService $companyUserCRUDService,
-        private AutoAttendanceService $autoAttendanceService
+        private CompanyUserCRUDService $companyUserCRUDService
     ) {
     }
 
@@ -32,9 +30,6 @@ class AssignRoleCompanyUserHandler
         $this->repository->assignRoleCompanyUser($assignRoleCompanyUserCommand->getId(), $assignRoleCompanyUserCommand->toArray(),$assignRoleCompanyUserCommand->getBranchIds());
         $companyUser = $this->repository->findOneBy(["id"=>$assignRoleCompanyUserCommand->getId()]);
         $userInCompany = $this->userRepository->findOneBy(["global_company_user_id" => $companyUser->global_id, "company_id" => $assignRoleCompanyUserCommand->getCompanyId()]);
-
-        $this->autoAttendanceService->generateAttendanceUsers($assignRoleCompanyUserCommand->getCompanyId());
-
         $data = [
             "name" => $userInCompany->name,
             "company_name" => $userInCompany->company?->name,
