d60e85555533f83d238a72bb733bac9228fd6935
AmrSaleh2234 <amrsaleh1001@gmail.com>
2025-08-27 10:40:35 +0300

    present residence in client presenter

--- modules/CompanyUser/Presenters/ClientPresenter.php
+++ modules/CompanyUser/Presenters/ClientPresenter.php
@@ -33,6 +33,7 @@ class ClientPresenter extends AbstractPresenter
             "branches"=>ManagementHierarchyPresenter::collection($this->user->managementHierarchies($this->role)->get()),
             "status" => $status  == "نشط" ? 1:0,
             "type"=>$this->user->clientDetail?->type,
+            "residence"=>$this->user?->companyUser?->residence,
             "broker_id"=>$this->user->clientDetail?->broker_id,
             "broker"=>$this->user->clientDetail?->broker_id !=null?["id"=>$this->user->clientDetail?->broker?->id,"name"=>$this->user->clientDetail?->broker?->name]:null,
             "company_representative_name"=>$this->user->clientDetail?->company_representative_name,
