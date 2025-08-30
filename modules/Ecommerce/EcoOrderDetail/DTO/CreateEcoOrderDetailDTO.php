b0f98d5a5a0c46287ce3f31f4592b1132d8486cd
Abdelrahman El-Desoky <dev.deesoky@gmail.com>
2025-08-30 15:33:29 +0300

    add culomns order

---
+++ modules/Ecommerce/EcoOrderDetail/DTO/CreateEcoOrderDetailDTO.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Modules\Ecommerce\EcoOrderDetail\DTO;
+
+use Ramsey\Uuid\UuidInterface;
+
+class CreateEcoOrderDetailDTO
+{
+    public function __construct(
+        public string $name,
+    ) {
+    }
+
+    public function toArray(): array
+    {
+        return [
+            'name' => $this->name,
+        ];
+    }
+}
