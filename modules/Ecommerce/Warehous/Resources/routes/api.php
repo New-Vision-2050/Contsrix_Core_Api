fe68d18329be07ba1a9649cffb70e49be92d273c
Abdelrahman El-Desoky <dev.deesoky@gmail.com>
2025-09-07 15:20:36 +0300

    add Statistics in Warehous

--- modules/Ecommerce/Warehous/Resources/routes/api.php
+++ modules/Ecommerce/Warehous/Resources/routes/api.php
@@ -6,6 +6,7 @@ use Modules\Ecommerce\Warehous\Controllers\WarehousController;
 Route::middleware(['auth:api',\Stancl\Tenancy\Middleware\InitializeTenancyByRequestData::class])->group(callback: function () {
     Route::get('/', [WarehousController::class, 'index']);
     Route::post('/', [WarehousController::class, 'store']);
+    Route::get('/statistics', [WarehousController::class, 'getStatistics']);
     Route::get('/{id}', [WarehousController::class, 'show']);
     Route::put('/{id}', [WarehousController::class, 'update']);
     Route::delete('/{id}', [WarehousController::class, 'delete']);
