981d387d745ed5ddbfca641db4d87afed094806c
Abdelrahman El-Desoky <dev.deesoky@gmail.com>
2025-09-22 13:47:38 +0300

    add banner setting

--- modules/Ecommerce/EcoAppSetting/Repositories/EcoBannerSettingRepository.php
+++ modules/Ecommerce/EcoAppSetting/Repositories/EcoBannerSettingRepository.php
@@ -22,10 +22,28 @@ class EcoBannerSettingRepository extends BaseRepository
         return $this->model->where('company_id', $companyId)->first();
     }
 
+    public function findByCompanyAndTypePage(string $companyId, ?string $typePage): ?EcoBannerSetting
+    {
+        return $this->model->where('company_id', $companyId)
+            ->where('type_page', $typePage)
+            ->first();
+    }
+
     public function upsertByCompanyId(string $companyId, array $data): EcoBannerSetting
     {
         return $this->model->updateOrCreate(
             ['company_id' => $companyId],
+            $data
+        );
+    }
+
+    public function upsertByCompanyAndTypePage(string $companyId, ?string $typePage, array $data): EcoBannerSetting
+    {
+        return $this->model->updateOrCreate(
+            [
+                'company_id' => $companyId,
+                'type_page' => $typePage
+            ],
             $data
         );
     }
