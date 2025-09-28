019a18b813a58857b64e30bee537afb2a6cb3296
AmrSaleh2234 <amrsaleh1001@gmail.com>
2025-09-28 21:23:54 +0300

    prevent to add

--- modules/NotificationSettings/Resources/routes/api.php
+++ modules/NotificationSettings/Resources/routes/api.php
@@ -5,8 +5,8 @@ use Modules\NotificationSettings\Controllers\NotificationSettingsController;
 
 Route::group(['middleware' => ['auth:api',\Stancl\Tenancy\Middleware\InitializeTenancyByRequestData::class]], function () {
 //    Route::get('/', [NotificationSettingsController::class, 'index']);
-    Route::post('/', [NotificationSettingsController::class, 'store']);
-    Route::post('/export', [NotificationSettingsController::class, 'export']);
+//    Route::post('/', [NotificationSettingsController::class, 'store']);
+//    Route::post('/export', [NotificationSettingsController::class, 'export']);
 
     Route::get('/', [NotificationSettingsController::class, 'show']);
     Route::put('/', [NotificationSettingsController::class, 'update']);
