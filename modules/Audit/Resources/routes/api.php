6592f7105c2505145e3f4f071a31df7a0e8827be
AmrSaleh2234 <amrsaleh1001@gmail.com>
2025-10-05 21:15:59 +0300

    activity logs

--- modules/Audit/Resources/routes/api.php
+++ modules/Audit/Resources/routes/api.php
@@ -5,6 +5,7 @@ use Modules\Audit\Controllers\AuditController;
 
 Route::group(['middleware' => ['auth:api',\Stancl\Tenancy\Middleware\InitializeTenancyByRequestData::class]], function () {
     Route::get('/', [AuditController::class, 'index']);
+    Route::get('/activity-log', [AuditController::class, 'activityLog']);
     Route::get('/{id}', [AuditController::class, 'show']);
     Route::delete('/{id}', [AuditController::class, 'delete']);
 });
