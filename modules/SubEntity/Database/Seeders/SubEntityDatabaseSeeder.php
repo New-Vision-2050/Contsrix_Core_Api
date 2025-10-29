ca93c0a1f6d8b02dfedca9425d9d0592b67681f3
AmrSaleh2234 <amrsaleh1001@gmail.com>
2025-10-29 14:57:20 +0300

    .

--- modules/SubEntity/Database/Seeders/SubEntityDatabaseSeeder.php
+++ modules/SubEntity/Database/Seeders/SubEntityDatabaseSeeder.php
@@ -5,6 +5,8 @@ declare(strict_types=1);
 namespace Modules\SubEntity\Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Modules\Program\Models\Program;
+use Modules\SubEntity\Models\RegistrationForm;
 use Ranium\SeedOnce\Traits\SeedOnce;
 use Modules\SubEntity\Models\SubEntity;
 
@@ -13,8 +15,33 @@ class SubEntityDatabaseSeeder extends Seeder
     use SeedOnce;
     public function run(): void
     {
-//        SubEntity::factory()
-//            ->count(50)
-//            ->create();
+        $superEntityType = fake()->randomElement(['users']);
+
+        SubEntity::factory([
+                'super_entity' => $superEntityType,
+                'origin_super_entity' => $superEntityType,
+                "name"=>"الموظفين",
+                "slug" => "employees",
+
+                'main_program_id' => Program::where("slug","human-resources")->first(),
+                'is_active' => 1,
+                'is_registrable' => 1,
+                'default_attributes' => json_encode([
+                    'name',
+                    'email',
+                    'phone',
+                ]),'optional_attributes' => json_encode([
+                    'name',
+                    'email',
+                    'phone',
+                ]),
+
+                'registration_form_id' => RegistrationForm::where("slug","employee")->first()
+            ]
+        )
+            ->count(1)
+            ->create();
+
+
     }
 }
