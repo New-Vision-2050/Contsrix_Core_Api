0a61622a08d01e709060b50b49a083dcb4639a11
AmrSaleh2234 <amrsaleh1001@gmail.com>
2025-11-26 20:44:04 +0300

    stop apis create ,delete

--- modules/WebsiteCMS/WebsiteTheme/Resources/routes/api.php
+++ modules/WebsiteCMS/WebsiteTheme/Resources/routes/api.php
@@ -5,14 +5,14 @@ use Modules\WebsiteCMS\WebsiteTheme\Controllers\WebsiteThemeController;
 
 Route::group(['middleware' => ['auth:api', \Stancl\Tenancy\Middleware\InitializeTenancyByRequestData::class]], function () {
     Route::get('/', [WebsiteThemeController::class, 'index']);
-    Route::post('/', [WebsiteThemeController::class, 'store']);
-    Route::post('/export', [WebsiteThemeController::class, 'export']);
+//    Route::post('/', [WebsiteThemeController::class, 'store']);
+//    Route::post('/export', [WebsiteThemeController::class, 'export']);
 
     // Current company theme routes
     Route::get('/current-company', [WebsiteThemeController::class, 'getCurrentCompanyTheme']);
     Route::post('/current-company', [WebsiteThemeController::class, 'updateCurrentCompanyTheme']);
 
-    Route::get('/{id}', [WebsiteThemeController::class, 'show']);
+//    Route::get('/{id}', [WebsiteThemeController::class, 'show']);
     Route::put('/{id}', [WebsiteThemeController::class, 'update']);
-    Route::delete('/{id}', [WebsiteThemeController::class, 'delete']);
+//    Route::delete('/{id}', [WebsiteThemeController::class, 'delete']);
 });
