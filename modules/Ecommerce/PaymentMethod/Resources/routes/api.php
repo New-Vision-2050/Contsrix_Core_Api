9769ccb7f715cd537afb37299fc77c4d5524c1dd
Abdelrahman El-Desoky <dev.deesoky@gmail.com>
2025-12-09 19:28:24 +0200

    add permission/ecommerce in payment-method

--- modules/Ecommerce/PaymentMethod/Resources/routes/api.php
+++ modules/Ecommerce/PaymentMethod/Resources/routes/api.php
@@ -2,16 +2,17 @@
 
 use Illuminate\Support\Facades\Route;
 use Modules\Ecommerce\PaymentMethod\Controllers\PaymentMethodController;
+use Modules\RoleAndPermission\Enums\Permission;
 
 Route::group(['middleware' => ['auth:api']], function () {
-    Route::get('/', [PaymentMethodController::class, 'index']);
-    Route::post('/', [PaymentMethodController::class, 'store']);
-    Route::post('/export', [PaymentMethodController::class, 'export']);
+    Route::get('/', [PaymentMethodController::class, 'index'])->permission(Permission::ECOMMERCE_PAYMENT_METHOD_LIST());
+    // Route::post('/', [PaymentMethodController::class, 'store'])->permission(Permission::ECOMMERCE_PAYMENT_METHOD_CREATE());
+    // Route::post('/export', [PaymentMethodController::class, 'export'])->permission(Permission::ECOMMERCE_PAYMENT_METHOD_EXPORT());
 
-    Route::get('/{id}', [PaymentMethodController::class, 'show']);
-    Route::put('/{id}', [PaymentMethodController::class, 'update']);
-    Route::delete('/{id}', [PaymentMethodController::class, 'delete']);
+    // Route::get('/{id}', [PaymentMethodController::class, 'show'])->permission(Permission::ECOMMERCE_PAYMENT_METHOD_VIEW());
+    // Route::put('/{id}', [PaymentMethodController::class, 'update'])->permission(Permission::ECOMMERCE_PAYMENT_METHOD_UPDATE());
+    // Route::delete('/{id}', [PaymentMethodController::class, 'delete'])->permission(Permission::ECOMMERCE_PAYMENT_METHOD_DELETE());
     
     // Toggle status route
-    Route::patch('/{type}/toggle-status', [PaymentMethodController::class, 'toggleStatus']);
+    Route::patch('/{type}/toggle-status', [PaymentMethodController::class, 'toggleStatus'])->permission(Permission::ECOMMERCE_PAYMENT_METHOD_ACTIVATE());
 });
