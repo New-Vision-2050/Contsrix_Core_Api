d7cd70189d9ec3814964fd2b2952e6a48bbb8b0c
AmrSaleh2234 <amrsaleh1001@gmail.com>
2025-12-11 16:57:24 +0300

    no authintication

--- modules/WebsiteCMS/WebsiteProjectSetting/Resources/routes/api.php
+++ modules/WebsiteCMS/WebsiteProjectSetting/Resources/routes/api.php
@@ -4,16 +4,22 @@ use Illuminate\Support\Facades\Route;
 use Modules\WebsiteCMS\WebsiteProjectSetting\Controllers\WebsiteProjectSettingController;
 use Modules\RoleAndPermission\Enums\Permission;
 
+Route::get('/', [WebsiteProjectSettingController::class, 'index'])->middleware([
+    \Stancl\Tenancy\Middleware\InitializeTenancyByRequestData::class
+]);
+
+Route::get('/{id}', [WebsiteProjectSettingController::class, 'show'])
+    ->middleware([
+        \Stancl\Tenancy\Middleware\InitializeTenancyByRequestData::class
+    ]);
 Route::group(['middleware' => ['auth:api',\Stancl\Tenancy\Middleware\InitializeTenancyByRequestData::class]], function () {
-    Route::get('/', [WebsiteProjectSettingController::class, 'index'])
-        ->permission(Permission::WEBSITE_PROJECT_SETTING_LIST());
+
     Route::post('/', [WebsiteProjectSettingController::class, 'store'])
         ->permission(Permission::WEBSITE_PROJECT_SETTING_CREATE());
     Route::post('/export', [WebsiteProjectSettingController::class, 'export'])
         ->permission(Permission::WEBSITE_PROJECT_SETTING_EXPORT());
 
-    Route::get('/{id}', [WebsiteProjectSettingController::class, 'show'])
-        ->permission(Permission::WEBSITE_PROJECT_SETTING_UPDATE());
+
     Route::put('/{id}', [WebsiteProjectSettingController::class, 'update'])
         ->permission(Permission::WEBSITE_PROJECT_SETTING_UPDATE());
     Route::delete('/{id}', [WebsiteProjectSettingController::class, 'delete'])
