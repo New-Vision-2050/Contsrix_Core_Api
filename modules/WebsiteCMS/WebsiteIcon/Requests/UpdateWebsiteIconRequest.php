93344dc82d6b82b7bfe346f41a618765cca171b3
AmrSaleh2234 <amrsaleh1001@gmail.com>
2025-12-24 14:23:55 +0300

    name in website icon is unique

--- modules/WebsiteCMS/WebsiteIcon/Requests/UpdateWebsiteIconRequest.php
+++ modules/WebsiteCMS/WebsiteIcon/Requests/UpdateWebsiteIconRequest.php
@@ -4,8 +4,10 @@ declare(strict_types=1);
 
 namespace Modules\WebsiteCMS\WebsiteIcon\Requests;
 
+use App\Rules\UniqueTranslationRule;
 use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Validation\Rule;
+use Modules\WebsiteCMS\WebsiteIcon\Models\WebsiteIcon;
 use Ramsey\Uuid\Uuid;
 use Modules\WebsiteCMS\WebsiteIcon\Commands\UpdateWebsiteIconCommand;
 use Modules\WebsiteCMS\WebsiteIcon\Handlers\UpdateWebsiteIconHandler;
@@ -16,8 +18,8 @@ class UpdateWebsiteIconRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'name_ar' => 'required|string|max:255',
-            'name_en' => 'required|string|max:255',
+            'name_ar' => ['required','string','max:255',new UniqueTranslationRule(new WebsiteIcon(),'name','ar',$this->route('id'))],
+            'name_en' => ['required','string','max:255',new UniqueTranslationRule(new WebsiteIcon(),'name','en',$this->route('id'))],
             'icon' => 'nullable|image|mimes:jpeg,png,jpg,gif,svg,webp|max:2048',
             'website_icon_category_type' => ['required', 'string', Rule::in(WebsiteIconCategoryType::values())],
         ];
