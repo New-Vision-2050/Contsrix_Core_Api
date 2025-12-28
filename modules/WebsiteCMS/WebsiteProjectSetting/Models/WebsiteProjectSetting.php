be4135fdfe9096065c62813df8d1206fe2f38fd5
AmrSaleh2234 <amrsaleh1001@gmail.com>
2025-12-28 15:48:16 +0300

    count prjects

--- modules/WebsiteCMS/WebsiteProjectSetting/Models/WebsiteProjectSetting.php
+++ modules/WebsiteCMS/WebsiteProjectSetting/Models/WebsiteProjectSetting.php
@@ -7,6 +7,7 @@ namespace Modules\WebsiteCMS\WebsiteProjectSetting\Models;
 use BasePackage\Shared\Traits\UuidTrait;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Modules\WebsiteCMS\WebsiteProject\Models\WebsiteProject;
 use Modules\WebsiteCMS\WebsiteProjectSetting\Database\factories\WebsiteProjectSettingFactory;
 use BasePackage\Shared\Traits\BaseFilterable;
 use BasePackage\Shared\Traits\HasTranslations;
@@ -19,10 +20,11 @@ class WebsiteProjectSetting extends Model
     use BaseFilterable;
     use HasTranslations;
     use BelongsToTenant;
+
     //use SoftDeletes;
 
     protected array $translatable = ['name'];
-
+    protected $withCount = ['websiteProjects'];
 
     public $incrementing = false;
 
@@ -42,5 +44,10 @@ class WebsiteProjectSetting extends Model
     protected static function newFactory(): WebsiteProjectSettingFactory
     {
         return WebsiteProjectSettingFactory::new();
+    }
+
+    public function websiteProjects()
+    {
+        return $this->hasMany(WebsiteProject::class);
     }
 }
