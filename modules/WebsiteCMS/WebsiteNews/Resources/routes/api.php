fb26b69c4518aacbea8e6fed8415ad364ac75e9c
AmrSaleh2234 <amrsaleh1001@gmail.com>
2025-12-28 21:23:34 +0300

    change method

--- modules/WebsiteCMS/WebsiteNews/Resources/routes/api.php
+++ modules/WebsiteCMS/WebsiteNews/Resources/routes/api.php
@@ -15,8 +15,7 @@ Route::group(['middleware' => ['auth:api', \Stancl\Tenancy\Middleware\Initialize
 
     Route::post('/{id}', [WebsiteNewsController::class, 'update'])
         ->permission(Permission::WEBSITE_NEWS_UPDATE());
-    Route::put('/{id}/toggle-active', [WebsiteNewsController::class, 'toggleStatus'])
-        ->permission(Permission::WEBSITE_NEWS_UPDATE());
+    Route::patch('/{id}/toggle-active', [WebsiteNewsController::class, 'toggleStatus']);
     Route::delete('/{id}', [WebsiteNewsController::class, 'delete'])
         ->permission(Permission::WEBSITE_NEWS_DELETE());
 });
