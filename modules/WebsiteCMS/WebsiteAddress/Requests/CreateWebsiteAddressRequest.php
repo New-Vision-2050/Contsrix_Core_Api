905d05a8c2f641a577cf44cb39c56fbe61a8edc1
AmrSaleh2234 <amrsaleh1001@gmail.com>
2025-12-28 14:02:46 +0300

    accept new attribute address

--- modules/WebsiteCMS/WebsiteAddress/Requests/CreateWebsiteAddressRequest.php
+++ modules/WebsiteCMS/WebsiteAddress/Requests/CreateWebsiteAddressRequest.php
@@ -15,6 +15,7 @@ class CreateWebsiteAddressRequest extends FormRequest
         return [
             'title_ar' => 'required|string|max:255',
             'title_en' => 'required|string|max:255',
+            'address' => 'required|string',
             'latitude' => 'nullable|numeric|between:-90,90',
             'longitude' => 'nullable|numeric|between:-180,180',
             'status' => 'nullable|integer|in:0,1',
@@ -28,6 +29,7 @@ class CreateWebsiteAddressRequest extends FormRequest
                 'ar' => $this->get('title_ar'),
                 'en' => $this->get('title_en'),
             ],
+            address: $this->get('address'),
             latitude:(float) $this->get('latitude'),
             longitude: (float) $this->get('longitude'),
             status: (int)$this->get('status', 1),
--- modules/WebsiteCMS/WebsiteAddress/Requests/UpdateWebsiteAddressRequest.php
+++ modules/WebsiteCMS/WebsiteAddress/Requests/UpdateWebsiteAddressRequest.php
@@ -16,6 +16,7 @@ class UpdateWebsiteAddressRequest extends FormRequest
         return [
             'title_ar' => 'required|string|max:255',
             'title_en' => 'required|string|max:255',
+            'address' => 'required|string',
             'latitude' => 'nullable|numeric|between:-90,90',
             'longitude' => 'nullable|numeric|between:-180,180',
             'status' => 'nullable|integer|in:0,1',
@@ -30,6 +31,7 @@ class UpdateWebsiteAddressRequest extends FormRequest
                 'ar' => $this->get('title_ar'),
                 'en' => $this->get('title_en'),
             ],
+            address: $this->get('address'),
             latitude:(float) $this->get('latitude'),
             longitude: (float) $this->get('longitude'),
             status: (int)$this->get('status', 1),
