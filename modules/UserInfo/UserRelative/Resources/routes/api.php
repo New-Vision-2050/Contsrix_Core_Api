38016b3ceed11e6dd43caa90e2d0a53f9b9ef419
AmrSaleh2234 <amrsaleh1001@gmail.com>
2026-02-10 22:09:12 +0300

    enable create api

--- modules/UserInfo/UserRelative/Resources/routes/api.php
+++ modules/UserInfo/UserRelative/Resources/routes/api.php
@@ -6,8 +6,8 @@ use Modules\RoleAndPermission\Enums\Permission;
 
 Route::group(['middleware' => ['auth:api',\Stancl\Tenancy\Middleware\InitializeTenancyByRequestData::class]], function () {
     Route::get('/user/{id}', [UserRelativeController::class, 'index'])->permission(Permission::PROFILE_MARITAL_STATUS_VIEW());
-//    Route::post('/', [UserRelativeController::class, 'store'])->permission(Permission::PROFILE_MARITAL_STATUS_CREATE());
+    Route::post('/', [UserRelativeController::class, 'store'])->permission(Permission::PROFILE_MARITAL_STATUS_CREATE());
     Route::get('/{id}', [UserRelativeController::class, 'show'])->permission(Permission::PROFILE_MARITAL_STATUS_VIEW());
-    Route::put('/{id}', [UserRelativeController::class, 'store'])->permission(Permission::PROFILE_MARITAL_STATUS_UPDATE());
+    Route::put('/{id}', [UserRelativeController::class, 'update'])->permission(Permission::PROFILE_MARITAL_STATUS_UPDATE());
     Route::delete('/{id}', [UserRelativeController::class, 'delete'])->permission(Permission::PROFILE_MARITAL_STATUS_DELETE());
 });
