d195698e2cf09270a69a2ecab663a7cae2fbba43
AmrSaleh2234 <amrsaleh1001@gmail.com>
2026-02-17 22:12:59 +0300

    fix assign role

--- modules/CompanyUser/Presenters/CompanyUserPresenter.php
+++ modules/CompanyUser/Presenters/CompanyUserPresenter.php
@@ -46,7 +46,7 @@ class CompanyUserPresenter extends AbstractPresenter
                     $this->companyUser
                 ))->getData()
                 : null,
-            "client_companies" => CompanyUsersPresenter::collection($this->companyUser->clientCompanies->unique('id'),$this->companyUser),
+//            "client_companies" => CompanyUsersPresenter::collection($this->companyUser->clientCompanies->unique('id'),$this->companyUser),
             "companies" => CompanyUsersPresenter::collection($this->companyUser->companies->unique('id'),$this->companyUser),
             'Job_role' => '-',
             'date_appointment' => '-',
