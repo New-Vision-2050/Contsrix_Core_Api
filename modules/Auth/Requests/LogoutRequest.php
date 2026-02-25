f739536132fc0dad163de899c0147528450da6e3
Abdelrahman El-Desoky <dev.deesoky@gmail.com>
2026-02-25 18:46:14 +0200

    fix logout

--- modules/Auth/Requests/LogoutRequest.php
+++ modules/Auth/Requests/LogoutRequest.php
@@ -5,24 +5,12 @@ declare(strict_types=1);
 namespace Modules\Auth\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
-use Modules\Auth\Commands\ForgetPasswordCommand;
-use Modules\Auth\DTO\LoginDTO;
-use Ramsey\Uuid\Uuid;
-use Modules\Auth\DTO\CreateAuthDTO;
 
 class LogoutRequest extends FormRequest
 {
     public function rules(): array
     {
-        return [
-            "email"=>"required|email",
-
-        ];
-    }
-
-    public function LogoutCommand()
-    {
-        return new  ForgetPasswordCommand(email: $this->get("email"));
+        return [];
     }
 
 
