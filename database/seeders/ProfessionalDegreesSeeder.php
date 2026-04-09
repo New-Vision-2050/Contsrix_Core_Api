f95c0de74310763651b501fb7b55d5f342055380
AmrSaleh2234 <amrsaleh1001@gmail.com>
2026-04-09 16:27:41 +0300

    profissional degress

--- database/seeders/ProfessionalDegreesSeeder.php
+++ database/seeders/ProfessionalDegreesSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
+use Ranium\SeedOnce\Traits\SeedOnce;
 
 class ProfessionalDegreesSeeder extends Seeder
 {
