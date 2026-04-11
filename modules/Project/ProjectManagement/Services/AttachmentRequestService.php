af4b3c932c930c3db57559a67ee1b62cf24501e4
AmrSaleh2234 <amrsaleh1001@gmail.com>
2026-04-11 22:25:36 +0300

    pull

--- modules/Project/ProjectManagement/Services/AttachmentRequestService.php
+++ modules/Project/ProjectManagement/Services/AttachmentRequestService.php
@@ -159,7 +159,6 @@ class AttachmentRequestService
             'decline' => 'attachment_declined',
             'request_update' => 'attachment_update_requested',
         ];
-        return                 $this->saveAttachmentToFolder($item);
 
 
         switch ($action) {
@@ -349,7 +348,6 @@ class AttachmentRequestService
 
         // Get or create folder structure
         $folderId = $this->getOrCreateFolderPath($request);
-        return $folderId;
 
         if (!$folderId) {
             // If no folder structure, save to project root folder
