67f8e9f9645f1201a9fc52e7ef67258b82b95ccc
AmrSaleh2234 <amrsaleh1001@gmail.com>
2026-04-25 22:24:30 +0300

    reverse dates

--- routes/console.php
+++ routes/console.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Console\Commands\CreateWaitingAttendanceCommand;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Schedule;
@@ -14,10 +15,24 @@ Schedule::command('notifications:send-document-notifications')
     ->timezone('Asia/Riyadh')
     ->withoutOverlapping()
     ->appendOutputTo(storage_path('logs/document-notifications.log'));
-    
-    Schedule::command('attendance:send-silent-notifications')
+
+Schedule::command(CreateWaitingAttendanceCommand::class)
+    ->everyThreeHours()
+    ->timezone('Asia/Riyadh')
+    ->withoutOverlapping()
+    ->appendOutputTo(storage_path('logs/attendance-waiting.log'));
+
+
+Schedule::command('attendance:auto-close-stale-shifts')
+    ->everyFiveMinutes()
+    ->timezone('Asia/Riyadh')
+    ->withoutOverlapping()
+    ->appendOutputTo(storage_path('logs/attendance-auto-close-stale-shifts.log'));
+
+
+
+Schedule::command('attendance:send-silent-notifications')
     ->everyFiveMinutes()
     ->timezone('Asia/Riyadh')
     ->withoutOverlapping()
     ->appendOutputTo(storage_path('logs/attendance-silent-notifications.log'));
-
