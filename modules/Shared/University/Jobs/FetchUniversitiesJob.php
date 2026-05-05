985bf09bbd7c946e402cce3fa423ac7f27a656cd
Abdelrahman El-Desoky <dev.desoky@gmail.com>
2026-05-05 18:34:43 +0300

    refactor seed UniversitiesSeederTableSeeder

---
+++ modules/Shared/University/Jobs/FetchUniversitiesJob.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Modules\Shared\University\Jobs;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Artisan;
+
+class FetchUniversitiesJob implements ShouldQueue
+{
+    use Dispatchable;
+    use InteractsWithQueue;
+    use Queueable;
+    use SerializesModels;
+
+    public function handle(): void
+    {
+        Artisan::call('universities:fetch');
+    }
+}
