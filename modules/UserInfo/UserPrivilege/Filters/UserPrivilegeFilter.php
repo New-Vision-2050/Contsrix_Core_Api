80bc82650bd063d83e4e5a1b31983a605e3de8f2
Abdelrahman El-Desoky <dev.desoky@gmail.com>
2026-06-03 14:32:13 +0300

    add filter privilege type

--- modules/UserInfo/UserPrivilege/Filters/UserPrivilegeFilter.php
+++ modules/UserInfo/UserPrivilege/Filters/UserPrivilegeFilter.php
@@ -5,13 +5,27 @@ declare(strict_types=1);
 namespace Modules\UserInfo\UserPrivilege\Filters;
 
 use BasePackage\Shared\Filters\SearchModelFilter;
+use Modules\Shared\Privilege\Services\PrivilegeCardConfigService;
 
 class UserPrivilegeFilter extends SearchModelFilter
 {
-       public $relations = [];
+    public $relations = [];
 
-        public function name($name)
-        {
-            return $this->where('name', $name);
-        }
+    public function name($name)
+    {
+        return $this->where('name', $name);
+    }
+
+    public function type(string $type)
+    {
+        return $this->where(function ($query) use ($type) {
+            $query->whereHas('privilege', function ($privilegeQuery) use ($type) {
+                $privilegeQuery->where('type', $type);
+            });
+
+            if ($type === PrivilegeCardConfigService::TYPE_HEALTH_INSURANCE) {
+                $query->orWhereNotNull('medical_insurance_id');
+            }
+        });
+    }
 }
--- modules/UserInfo/UserPrivilege/Requests/GetUserPrivilegeListRequest.php
+++ modules/UserInfo/UserPrivilege/Requests/GetUserPrivilegeListRequest.php
@@ -5,7 +5,8 @@ declare(strict_types=1);
 namespace Modules\UserInfo\UserPrivilege\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
-use Ramsey\Uuid\Uuid;
+use Illuminate\Validation\Rule;
+use Modules\Shared\Privilege\Services\PrivilegeCardConfigService;
 
 class GetUserPrivilegeListRequest extends FormRequest
 {
@@ -14,6 +15,7 @@ class GetUserPrivilegeListRequest extends FormRequest
         return [
             'per_page' => 'integer',
             'page' => 'integer',
+            'type' => ['nullable', 'string', Rule::in(array_keys(app(PrivilegeCardConfigService::class)->allConfigs()))],
         ];
     }
 }
