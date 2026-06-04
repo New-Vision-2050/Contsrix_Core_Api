d4a68d7026e5499628da8712226f033b927d3dc2
AmrSaleh2234 <amrsaleh1001@gmail.com>
2026-06-04 16:45:58 +0300

    omit auth channel

--- routes/channels.php
+++ routes/channels.php
@@ -7,9 +7,9 @@ Broadcast::channel('App.Models.User.{id}', function ($user, $id) {
 });
 
 // Employee Task inbox notifications - one channel per action taker
-Broadcast::channel('inbox.{userId}', function ($user, $userId) {
-    return (string) $user->id === (string) $userId;
-});
+//Broadcast::channel('inbox.{userId}', function ($user, $userId) {
+//    return (string) $user->id === (string) $userId;
+//});
 
 //// Allow users to listen to their company's channel
 //Broadcast::channel('company.{companyId}', function ($user, $companyId) {
