7d7959bfd8bb39d8d369e1117c10727e8a684536
AmrSaleh2234 <amrsaleh1001@gmail.com>
2026-06-10 17:03:56 +0300

    update date time

--- modules/Reports/Presenters/ReportPresenter.php
+++ modules/Reports/Presenters/ReportPresenter.php
@@ -40,10 +40,10 @@ class ReportPresenter extends AbstractPresenter
             'status'            => $this->report->status,
             'file_path'         => $this->report->file_path,
             'file_size'         => $this->report->file_size,
-            'generated_at'      => optional($this->report->generated_at)->toDateTimeString(),
+            'generated_at'      => optional($this->report->generated_at)->format('Y-m-d h:i:s A'),
             'error_message'     => $this->report->error_message,
-            'created_at'        => optional($this->report->created_at)->toDateTimeString(),
-            'updated_at'        => optional($this->report->updated_at)->toDateTimeString(),
+            'created_at'        => optional($this->report->created_at)->format('Y-m-d h:i:s A'),
+            'updated_at'        => optional($this->report->updated_at)->format('Y-m-d h:i:s A'),
         ];
     }
 }
