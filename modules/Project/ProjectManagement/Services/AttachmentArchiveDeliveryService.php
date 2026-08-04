54d6a441932749647d956f044bcc68bd96d91b78
AmrSaleh2234 <amrsaleh1001@gmail.com>
2026-08-04 11:11:45 +0300

    solve issue

--- modules/Project/ProjectManagement/Services/AttachmentArchiveDeliveryService.php
+++ modules/Project/ProjectManagement/Services/AttachmentArchiveDeliveryService.php
@@ -162,9 +162,9 @@ final class AttachmentArchiveDeliveryService
 
             $newMedia = new CustomMedia();
             $newMedia->forceFill($attrs);
-            $newMedia->custom_properties = is_string($newMedia->getRawAttribute('custom_properties'))
-                ? json_decode($newMedia->getRawAttribute('custom_properties'), true) ?? []
-                : ($newMedia->custom_properties ?? []);
+            $newMedia->custom_properties = is_string($attrs['custom_properties'] ?? null)
+                ? json_decode($attrs['custom_properties'], true) ?? []
+                : ($attrs['custom_properties'] ?? []);
             $newMedia->uuid = Uuid::uuid4()->toString();
             $newMedia->model_id = $file->id;
             $newMedia->model_type = File::class;
