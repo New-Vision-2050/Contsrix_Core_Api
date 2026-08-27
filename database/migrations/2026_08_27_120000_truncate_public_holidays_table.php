94894403b77af8e40c7d7d77c017828dfa80faaa
AmrSaleh2234 <amrsaleh1001@gmail.com>
2026-08-27 13:27:46 +0300

    truncate table

--- database/migrations/2026_08_27_120000_truncate_public_holidays_table.php
+++ database/migrations/2026_08_27_120000_truncate_public_holidays_table.php
@@ -7,8 +7,12 @@ return new class extends Migration
 {
     public function up(): void
     {
+        DB::statement('SET FOREIGN_KEY_CHECKS = 0;');
+
         DB::table('public_holiday_days')->truncate();
         DB::table('public_holidays')->truncate();
+
+        DB::statement('SET FOREIGN_KEY_CHECKS = 1;');
     }
 
     public function down(): void
