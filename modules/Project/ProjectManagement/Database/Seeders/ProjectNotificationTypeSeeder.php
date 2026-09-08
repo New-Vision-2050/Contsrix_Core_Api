b0f389214e8a9cbdce65148f82a1b666730730d5
Abdelrahman El-Desoky <dev.desoky@gmail.com>
2026-09-08 14:38:23 +0300

    add seeder

--- modules/Project/ProjectManagement/Database/Seeders/ProjectNotificationTypeSeeder.php
+++ modules/Project/ProjectManagement/Database/Seeders/ProjectNotificationTypeSeeder.php
@@ -20,6 +20,16 @@ class ProjectNotificationTypeSeeder extends Seeder
             ['name_ar' => 'جهد متوسط معدات ارضي', 'name_en' => 'Medium voltage ground equipment', 'type' => 'electricity'],
             ['name_ar' => 'جهد منخفض معدات ارضي', 'name_en' => 'Low voltage ground equipment', 'type' => 'electricity'],
             ['name_ar' => 'جهد منخفض معدات هوائي', 'name_en' => 'Low voltage aerial equipment', 'type' => 'electricity'],
+            ['name_ar' => 'كسر رئيسي', 'name_en' => 'Main break', 'type' => 'water'],
+            ['name_ar' => 'كسر خط فرعي', 'name_en' => 'Branch line break', 'type' => 'water'],
+            ['name_ar' => 'كسر خط رئيسي', 'name_en' => 'Main line break', 'type' => 'water'],
+            ['name_ar' => 'لم يتم العثور علي كسر', 'name_en' => 'No break found', 'type' => 'water'],
+            ['name_ar' => 'تغيير صمام', 'name_en' => 'Valve replacement', 'type' => 'water'],
+            ['name_ar' => 'كسر ماسورة فرعية', 'name_en' => 'Branch pipe break', 'type' => 'water'],
+            ['name_ar' => 'كسر قائم العداد', 'name_en' => 'Meter riser break', 'type' => 'water'],
+            ['name_ar' => 'تسريب بالعداد', 'name_en' => 'Meter leak', 'type' => 'water'],
+            ['name_ar' => 'انقطاع', 'name_en' => 'Interruption', 'type' => 'water'],
+            ['name_ar' => 'غسيل', 'name_en' => 'Flushing', 'type' => 'water'],
         ];
 
         foreach ($types as $index => $type) {
